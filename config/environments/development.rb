495f3d26d231b266ecd87ae64b32926f22f47ab2
hrl7 <hrlclb@gmail.com>
2015-09-02 17:42:19 +0900

    Fix browserify config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   #
-  config.browserify_rails.paths << /vendor\/assets\/javascripts\/module\.js/
+  config.browserify_rails.paths = [ /app\/assets\/javascripts\/application.js/,
+                                    /vendor\/assets\/javascripts\/module\.js/]
   config.browserify_rails.source_map_environments << "development"
   config.browserify_rails.evaluate_node_modules = true
   config.browserify_rails.force = true

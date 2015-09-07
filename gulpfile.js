b526c31434c99263e7a15a71b1b679aeb139eb40
hrl7 <hrlclb@gmail.com>
2015-09-07 13:35:02 +0900

    Replace gulp-browserify to gulp-watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var watchify = require('gulp-watchify');
 var reactify = require('reactify');
-var sourcemaps = require('gulp-sourcemaps');
 
 var SRC_CLIENT =  [
     'app/assets/javascripts/client/dispatcher/*',
     'app/assets/javascripts/client/stores/*',
     'app/assets/javascripts/client/constants/*',
     'app/assets/javascripts/client/actions/*',
-    'app/assets/javascripts/client/main.js',
-    'app/assets/javascripts/client/fabnavi.js',
-    'app/assets/javascripts/client/application.js'
+    'app/assets/javascripts/client/fabnavi.js'
     ] ;
 
 var DIST_CLIENT = "app/assets/javascripts/dist/client";
 
-gulp.task('default', function(){
-  gulp.src(SRC_CLIENT)
-  .pipe(browserify({
+gulp.task('default', watchify(function(watchify){
+  return gulp.src(SRC_CLIENT)
+  .pipe(watchify({
     transform : ['reactify'],
     extensions: ['.js'],
-    debug : true
+    debug : true,
+    watching: true
   }))
-  .pipe(gulp.dest(DIST_CLIENT))
-});
+  .pipe(gulp.dest(DIST_CLIENT));
+}));

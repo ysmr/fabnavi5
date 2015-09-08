ff3c27c3fd83bb089b03d595d97c3fabc3c00777
hrl7 <hrlclb@gmail.com>
2015-09-09 00:45:30 +0900

    Notify if build errors

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var plumber = require('gulp-plumber');
 var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var notifier = require('node-notifier');
 
 var browserify = require('browserify');
 var reactify = require('reactify');
@@ -41,6 +42,7 @@ function watch(){
     .on('error', function(e){
       //gutil.log('Browserify Error',_.omit(e,"stream"));
       gutil.log('Browserify Error',e);
+      notifier.notify({title:"Browserify Error", message: e.path.replace(__dirname+"/app/assets/javascripts/client","")});
     })
     .pipe(source("bundle.js"))
     .pipe(buffer())

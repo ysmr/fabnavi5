3430ecb18c2d049ad9f3d584b2d5f3f18d39b486
johnny <takuma.miyake@framgia.com>
2016-03-05 20:47:35 -0800

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     mount_devise_token_auth_for 'User', at: '/v1/auth'
     mount API => '/'
-    mount GrapeSwaggerRails::Engine => '/v1/swagger'
+    if Rails.env.development?
+      mount GrapeSwaggerRails::Engine => '/v1/swagger'
+    end
   end
 end

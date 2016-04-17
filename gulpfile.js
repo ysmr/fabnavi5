f3da116a633d29db212108758f9456261f3dab96
hrl7 <hrlclb@gmail.com>
2016-04-18 02:04:20 +0900

    show error log, if build failed

--- gulpfile.js
+++ gulpfile.js
@@ -75,13 +75,14 @@ function watch(){
 
 function build(){
   var b = browserify(options);
-  b.bundle(function(err, buf){
-    if(err)process.exit(1);
-  })
+  b.bundle()
   .on('log', gutil.log)
   .on('error',function(e){
     gutil.log('Browserify Error',e.message);
     gutil.log('Stack Trace:',e.stack);
+    setTimeout(function(){
+      process.exit(1);
+    },1000);
   })
   .pipe(source("bundle.js"))
   .pipe(buffer())

eef8770526f8feb5f8dbf5a502f2fa8858a9b2fe
hrl7 <hrlclb@gmail.com>
2016-04-18 23:35:09 +0900

    Cleanup stores/*

--- app/assets/javascripts/client/stores/ProjectStore.js
+++ app/assets/javascripts/client/stores/ProjectStore.js
@@ -1,4 +1,3 @@
-
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events');
 var EventTypes = require('../constants/EventTypes');
@@ -25,295 +24,294 @@ function getStep( ){
 }
 
 var ProjectStore = Object.assign({}, EventEmitter.prototype, {
- init : function () {
-  _project = null;
-  _current_page = 0;
-  _uploadQueue = [];
-  _shooting = false;
-  Camera.init();
- },
+  init : function () {
+    _project = null;
+    _current_page = 0;
+    _uploadQueue = [];
+    _shooting = false;
+    Camera.init();
+  },
 
- shoot : function(){
-   console.log("shoot");
-   if(_shooting){
-    console.log("shooting now.");
-    return;
-   }
-   ProjectStore.emitClearCanvas();
-   Camera.shoot().then(function(url){
-     var fig = ProjectStore.newFigure();
-     ProjectStore.setImageToFigureFromCamera(fig, url);
-     ProjectStore.pushFigure( fig );
-     fig.figure.clientContent.dfdImage
-       .then(ImageConverter.toBlob)
-       .then(function(blob){
-         if( url.length > 1000 ){
-           url = url.slice(30,40) + ".jpg";
-         }
+  shoot : function(){
+    console.log("shoot");
+    if(_shooting){
+      console.log("shooting now.");
+      return;
+    }
+    ProjectStore.emitClearCanvas();
+    Camera.shoot().then(function(url){
+      var fig = ProjectStore.newFigure();
+      ProjectStore.setImageToFigureFromCamera(fig, url);
+      ProjectStore.pushFigure( fig );
+      fig.figure.clientContent.dfdImage
+        .then(ImageConverter.toBlob)
+        .then(function(blob){
+          if( url.length > 1000 ){
+            url = url.slice(30, 40) + ".jpg";
+          }
 
-         var payload = {
-           file : blob,
-           name : url.replace(/\?.*/,"").replace(/^.*\//,""),
-           sym : fig.figure.sym,
-         };
-         ProjectActionCreator.uploadAttachment(payload);
-         payload.status = "Uploading";
-         ProjectStore.pushUploadQueue(payload);
-       });
-   });
- },
+          var payload = {
+            file : blob,
+            name : url.replace(/\?.*/, "").replace(/^.*\//, ""),
+            sym : fig.figure.sym,
+          };
+          ProjectActionCreator.uploadAttachment(payload);
+          payload.status = "Uploading";
+          ProjectStore.pushUploadQueue(payload);
+        });
+    });
+  },
 
- pushUploadQueue : function( payload ){
-   _uploadQueue.push({name:payload.name, sym:payload.sym, status: payload.status});
-   ProjectStore.emitChange();
- },
+  pushUploadQueue : function( payload ){
+    _uploadQueue.push({name:payload.name, sym:payload.sym, status: payload.status});
+    ProjectStore.emitChange();
+  },
 
- uploadFinish : function( sym ){
-   for(var i = 0; i<_uploadQueue.length; i++){
-     if(_uploadQueue[i].sym == sym){
-       _uploadQueue.splice(i,1);
-       ProjectStore.emitChange();
-     }
-   }
- },
+  uploadFinish : function( sym ){
+    for(var i = 0; i < _uploadQueue.length; i++){
+      if(_uploadQueue[i].sym == sym){
+        _uploadQueue.splice(i, 1);
+        ProjectStore.emitChange();
+      }
+    }
+  },
 
- uploadFailed : function( sym ){
-   for(var i = 0; i<_uploadQueue.length; i++){
-     if(_uploadQueue[i].sym == sym){
-       _uploadQueue[i].status = "Error";
-       ProjectStore.emitChange();
-     }
-   }
- },
+  uploadFailed : function( sym ){
+    for(var i = 0; i < _uploadQueue.length; i++){
+      if(_uploadQueue[i].sym == sym){
+        _uploadQueue[i].status = "Error";
+        ProjectStore.emitChange();
+      }
+    }
+  },
 
- next : function(){
-   ProjectStore.setPage(_current_page + 1);
- },
+  next : function(){
+    ProjectStore.setPage(_current_page + 1);
+  },
 
- prev : function(){
-   ProjectStore.setPage(_current_page - 1);
- },
+  prev : function(){
+    ProjectStore.setPage(_current_page - 1);
+  },
 
- setPage : function( page ){
+  setPage : function( page ){
+    if( !_project.hasOwnProperty("content") ) {
+      return ;
+    }
+    if( page >= _project.content.length ) {
+      page = _project.content.length - 1;
+    }
 
-   if( ! _project.hasOwnProperty("content") ) {
-     return ;
-   }
-   if( page >= _project.content.length ) {
-     page = _project.content.length -1 ;
-   }
+    if( page < 0 ) {
+      page = 0;
+    }
 
-   if( page < 0 ) {
-     page = 0;
-   }
-
-   _current_page = page;
-   console.log("page : ",page);
-   if(_project.content[page].figure.hasOwnProperty("_destroy") &&
+    _current_page = page;
+    console.log("page : ", page);
+    if(_project.content[page].figure.hasOwnProperty("_destroy") &&
       _project.content[page].figure._destroy){
-    console.log("******DELETE FLAG*********");
-   }
-   ProjectStore.emitChange();
- },
+      console.log("******DELETE FLAG*********");
+    }
+    ProjectStore.emitChange();
+  },
 
- setProject : function( project ){
-   _project = project;
-   this.emitChange();
- },
+  setProject : function( project ){
+    _project = project;
+    this.emitChange();
+  },
 
- pushFigure : function( fig ){
-   _project.content.push(fig);
-   ProjectStore.emitChange();
- },
+  pushFigure : function( fig ){
+    _project.content.push(fig);
+    ProjectStore.emitChange();
+  },
 
- mergeUploadedFigure : function( fig ){
-   var dst = ProjectStore.findFigureBySymbol( fig.sym );
-   dst.figure.id = fig.id;
-   dst.figure.file = fig.file;
-   ProjectStore.saveProject();
-   ProjectStore.uploadFinish( fig.sym );
-   ProjectStore.emitChange();
- },
+  mergeUploadedFigure : function( fig ){
+    var dst = ProjectStore.findFigureBySymbol( fig.sym );
+    dst.figure.id = fig.id;
+    dst.figure.file = fig.file;
+    ProjectStore.saveProject();
+    ProjectStore.uploadFinish( fig.sym );
+    ProjectStore.emitChange();
+  },
 
- saveProject : function(){
-   setTimeout(function(){
-     ProjectActionCreator.updateProject({
-       project :  ProjectStore.getProject()
-     });
-   },0);
- },
+  saveProject : function(){
+    setTimeout(function(){
+      ProjectActionCreator.updateProject({
+        project :  ProjectStore.getProject()
+      });
+    }, 0);
+  },
 
- toggleDestroy : function(){
-   if( _project.content[_current_page].figure.hasOwnProperty("_destroy") ){
-    _project.content[_current_page].figure._destroy = !_project.content[_current_page].figure._destroy;
-   } else {
-    _project.content[_current_page].figure["_destroy"] = true;
-   }
-   this.emitChange();
- },
+  toggleDestroy : function(){
+    if( _project.content[_current_page].figure.hasOwnProperty("_destroy") ){
+      _project.content[_current_page].figure._destroy = !_project.content[_current_page].figure._destroy;
+    } else {
+      _project.content[_current_page].figure["_destroy"] = true;
+    }
+    this.emitChange();
+  },
 
- newFigure : function( ){
-   return {
-     figure : {
-       sym           : gensym(),
-       figure_id     : null,
-       clientContent : {
-         dfdImage      : null,
-         dfdThumbnail  : null,
-       },
-       serverContent : {
-         dfdImage      : null,
-         dfdThumbnail  : null,
-       },
-       file : {
-         id : null,
-         file : {
-           url : null,
-           thumb : {
-             url : null
-           },
-         },
-       },
-       position : 7,
-     }
-   };
- },
+  newFigure : function( ){
+    return {
+      figure : {
+        sym           : gensym(),
+        figure_id     : null,
+        clientContent : {
+          dfdImage      : null,
+          dfdThumbnail  : null,
+        },
+        serverContent : {
+          dfdImage      : null,
+          dfdThumbnail  : null,
+        },
+        file : {
+          id : null,
+          file : {
+            url : null,
+            thumb : {
+              url : null
+            },
+          },
+        },
+        position : 7,
+      }
+    };
+  },
 
- mergeFigureId : function( project ){
+  mergeFigureId : function( project ){
     var res = project.project;
-    for(var i=0; i<res.content.length; i++){
-      for(var j=0; j<_project.content.length; j++){
-        if(_project.content[j].figure.figure_id == null &&  _project.content[j].figure.id == res.content[i].figure.id ){
+    for(var i = 0; i < res.content.length; i++){
+      for(var j = 0; j < _project.content.length; j++){
+        if(_project.content[j].figure.figure_id == null && _project.content[j].figure.id == res.content[i].figure.id ){
           _project.content[j].figure.figure_id = res.content[i].figure.figure_id;
         }
       }
     }
     ProjectActionCreator.setThumbnailLast({ project:_project });
- },
+  },
 
- setImageToFigureFromCamera : function( fig, url ){
-  var src = null;
-   if( url ){
-     src = url;
-     fig.figure.file.file.url = url;
-   } else if( fig.figure.file.file.url ) {
-     src = fig.figure.file.file.url;
-   } else {
-     throw new Error("Figure url is not set");
-   }
+  setImageToFigureFromCamera : function( fig, url ){
+    var src = null;
+    if( url ){
+      src = url;
+      fig.figure.file.file.url = url;
+    } else if( fig.figure.file.file.url ) {
+      src = fig.figure.file.file.url;
+    } else {
+      throw new Error("Figure url is not set");
+    }
 
-   var img = new Image();
-   var d = $.Deferred();
-   img.src = src;
-   img.crossOrigin='anonymous';
-   img.onload = function(){
-     d.resolve(img);
-   }
-   fig.figure.clientContent.dfdImage = d.promise();
- },
+    var img = new Image();
+    var d = $.Deferred();
+    img.src = src;
+    img.crossOrigin = 'anonymous';
+    img.onload = function(){
+      d.resolve(img);
+    }
+    fig.figure.clientContent.dfdImage = d.promise();
+  },
 
- setImageToFigureFromServer : function( fig, url ){
+  setImageToFigureFromServer : function( fig, url ){
 
- },
+  },
 
- setThumbnailToFigureFromServer : function( fig, url ){
+  setThumbnailToFigureFromServer : function( fig, url ){
 
- },
+  },
 
 
- emitChange : function(){
-   _shooting = false;
-   this.emit(EventTypes.PROJECT_CHANGE);
- },
+  emitChange : function(){
+    _shooting = false;
+    this.emit(EventTypes.PROJECT_CHANGE);
+  },
 
- emitUpdateCanvas : function(){
+  emitUpdateCanvas : function(){
     if( _project == null ){
       return;
     }
-   this.emit(EventTypes.UPDATE_CANVAS_REQUEST);
- },
+    this.emit(EventTypes.UPDATE_CANVAS_REQUEST);
+  },
 
- emitClearCanvas : function(){
-   _shooting = true;
-   this.emit(EventTypes.CLEAR_CANVAS_REQUEST);
- },
+  emitClearCanvas : function(){
+    _shooting = true;
+    this.emit(EventTypes.CLEAR_CANVAS_REQUEST);
+  },
 
- getProject : function( ){
-   return _project;
- },
+  getProject : function( ){
+    return _project;
+  },
 
- getCurrentPage: function(){
-   return _current_page;
- },
+  getCurrentPage: function(){
+    return _current_page;
+  },
 
- getUploadQueue : function(){
-   return _uploadQueue;
- },
+  getUploadQueue : function(){
+    return _uploadQueue;
+  },
 
- isShooting : function(){
-  return _shooting;
- },
+  isShooting : function(){
+    return _shooting;
+  },
 
- addChangeListener: function(callback) {
-   this.on(EventTypes.PROJECT_CHANGE, callback);
- },
+  addChangeListener: function(callback) {
+    this.on(EventTypes.PROJECT_CHANGE, callback);
+  },
 
- addCanvasRequestListener: function(callback) {
-   this.on(EventTypes.UPDATE_CANVAS_REQUEST, callback);
- },
+  addCanvasRequestListener: function(callback) {
+    this.on(EventTypes.UPDATE_CANVAS_REQUEST, callback);
+  },
 
- addCanvasClearListener: function(callback) {
-   this.addListener(EventTypes.CLEAR_CANVAS_REQUEST, callback);
- },
+  addCanvasClearListener: function(callback) {
+    this.addListener(EventTypes.CLEAR_CANVAS_REQUEST, callback);
+  },
 
- removeCanvasRequestListener: function(callback) {
-   this.on(EventTypes.UPDATE_CANVAS_REQUEST, callback);
- },
+  removeCanvasRequestListener: function(callback) {
+    this.on(EventTypes.UPDATE_CANVAS_REQUEST, callback);
+  },
 
- removeChangeListener: function(callback) {
-   this.removeListener(EventTypes.PROJECT_CHANGE, callback);
- },
+  removeChangeListener: function(callback) {
+    this.removeListener(EventTypes.PROJECT_CHANGE, callback);
+  },
 
- removeCanvasClearListener: function(callback) {
-   this.removeListener(EventTypes.CLEAR_CANVAS_REQUEST, callback);
- },
+  removeCanvasClearListener: function(callback) {
+    this.removeListener(EventTypes.CLEAR_CANVAS_REQUEST, callback);
+  },
 
- findFigureBySymbol : function( sym ){
-    var cts  = ProjectStore.getProject().content;
+  findFigureBySymbol : function( sym ){
+    var cts = ProjectStore.getProject().content;
     var fig = null;
     for(var i = 0; i < cts.length; i++){
-       fig = cts[i];
-       if( fig.figure.hasOwnProperty('sym') && fig.figure.sym == sym ) return fig;
+      fig = cts[i];
+      if( fig.figure.hasOwnProperty('sym') && fig.figure.sym == sym )return fig;
     }
- },
+  },
 });
 
 ProjectStore.dispatchToken = AppDispatcher.register(function( action ){
   switch(action.type){
     case KeyActionTypes.CALIBRATE_LONGER_HORIZONTAL:
-      CalibrateController.changeRegionCB(-STEP,0)();
+      CalibrateController.changeRegionCB(-STEP, 0)();
       break;
     case KeyActionTypes.CALIBRATE_SHORTER_HORIZONTAL:
-      CalibrateController.changeRegionCB(STEP,0)();
+      CalibrateController.changeRegionCB(STEP, 0)();
       break;
     case KeyActionTypes.CALIBRATE_LONGER_VERTICAL:
-      CalibrateController.changeRegionCB(0,STEP)();
+      CalibrateController.changeRegionCB(0, STEP)();
       break;
     case KeyActionTypes.CALIBRATE_SHORTER_VERTICAL:
-      CalibrateController.changeRegionCB(0,-STEP)();
+      CalibrateController.changeRegionCB(0, -STEP)();
       break;
 
     case KeyActionTypes.CALIBRATE_MOVE_RIGHT:
-      CalibrateController.moveRegionCB(STEP,0)();
+      CalibrateController.moveRegionCB(STEP, 0)();
       break;
     case KeyActionTypes.CALIBRATE_MOVE_LEFT:
-      CalibrateController.moveRegionCB(-STEP,0)();
+      CalibrateController.moveRegionCB(-STEP, 0)();
       break;
     case KeyActionTypes.CALIBRATE_MOVE_DOWN:
-      CalibrateController.moveRegionCB(0,STEP)();
+      CalibrateController.moveRegionCB(0, STEP)();
       break;
     case KeyActionTypes.CALIBRATE_MOVE_UP:
-      CalibrateController.moveRegionCB(0,-STEP)();
+      CalibrateController.moveRegionCB(0, -STEP)();
       break;
 
     case KeyActionTypes.PROJECT_SAVE:
@@ -321,7 +319,7 @@ ProjectStore.dispatchToken = AppDispatcher.register(function( action ){
         ProjectActionCreator.updateProject({
           project:ProjectStore.getProject()
         });
-      },0);
+      }, 0);
       break;
     case KeyActionTypes.TOGGLE_DELETE_FLAG :
       ProjectStore.toggleDestroy();
